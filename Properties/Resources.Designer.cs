55c13922d21e75cf63f4c5dea097f042adb10280
Hao Wu <hao.wu.au@gmail.com>
2023-08-20 09:20:35 +1000

    Rename the project

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OneButton.Properties {
+namespace FrameFreeze.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace OneButton.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OneButton.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FrameFreeze.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
